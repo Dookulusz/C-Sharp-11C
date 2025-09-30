5428d046641cb1de576d3591aaa764945d987ffb
Dookusz <daniel.doka@umszki.hu>
2025-09-30 10:52:19 +0200

    Bináris folytatás

--- Ciklus/Binaris/Program.cs
+++ Ciklus/Binaris/Program.cs
@@ -3,20 +3,26 @@
  */
 int deci;
 int maradék;
+string bináris = string.Empty;
 
 Console.Write("Add meg a számot: ");
 deci = int.Parse(Console.ReadLine());
 
 while (deci > 0)
 {
+    //számoljuk ki a maradékát
     maradék = deci % 2;
 
-    Console.WriteLine(maradék);
+    //fűzzük hozzá az eredményhez trükkfejmókus módon
+    bináris = maradék + bináris;
 
+    //(technikailag erre nincs szükség)
     deci = deci - maradék;
 
+    //felezzük meg a számot
     deci = deci / 2;
 }
+Console.Write(bináris);
 
 
 
