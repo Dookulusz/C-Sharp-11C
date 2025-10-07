7dfb88dc9cd532781141673a136befce52c8abfe
Dookusz <daniel.doka@umszki.hu>
2025-10-07 11:17:36 +0200

    Fix

---
+++ Ciklus2/For Ciklus/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+int szam = 1234567123;
+bool isPrime = true;
+
+for (int i = 2; i < szam + 1; i++)
+{
+    if (szam % i == 0)
+    {
+        isPrime = false;
+        Console.WriteLine($"Ezzel a számmal osztahtó: {i}");
+        break;
+    }
+    Console.WriteLine(i);
+}
+
+if (isPrime)
+{
+    Console.WriteLine("Prím");
+}
+else
+{
+    Console.WriteLine("nem pírm");
+}
+*/
+long egyik = 0;
+long masik = 0;
+
+for (int i = 0; i < 50; i++)
+{
+    long következő = egyik + masik;
+
+    egyik = masik;
+
+    masik = következő;
+}
+Console.WriteLine(egyik);
+Console.WriteLine(masik);
---
+++ Ciklus2/For Ciklus2/For Ciklus2/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+int col = 8;
+int row = 8;
+for (row = 0 ; row < 8; row++)
+{
+    for (col = 0; col < 8; col++)
+    {
+        if (col % 2 == row % 2)
+        {
+            Console.Write("■");
+        }
+        else
+        {
+            Console.Write(" ");
+        }
+
+    }
+    Console.WriteLine();
+}
+*/
+
+string palya = "";
+int meret = 64;
+
+for  (int i = 1; i != meret + 1; i++)
+{
+    if (i % 2 == 0)
+    {
+        palya += "☺";
+    }
+    else
+    {
+        palya += "☻";
+    }
+
+    if (i % 8 == 0)
+    {
+
+        palya += "\n";
+    }
+}
+Console.Write(palya);
