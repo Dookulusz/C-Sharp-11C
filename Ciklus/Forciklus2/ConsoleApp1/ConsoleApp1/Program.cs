2ed34393cec826643aff1b3b9a48724f2f68ae7e
Dookusz <daniel.doka@umszki.hu>
2025-10-08 11:52:32 +0200

    Prím

---
+++ Ciklus/Forciklus2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.Net.WebSockets;
+
+int szam = int.Parse(Console.ReadLine());
+int prime = 0;
+
+
+for (int i = 2; i < szam; i++)
+{
+
+    if (szam % i == 0)
+    {
+        Console.WriteLine($"{i}-vel/val oszahtó");
+        prime++;
+    }
+
+}
+Console.WriteLine(prime);
+if (prime == 0)
+{
+    Console.WriteLine("Prím");
+}
+else
+{
+    Console.WriteLine("Nem prím");
+}
+
+
+
