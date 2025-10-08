fe599fdcfcffc3ce5db4f1390bf78cacb33100dc
Dookusz <daniel.doka@umszki.hu>
2025-10-08 23:12:07 +0200

    Otthoni feladatom
    Sajnos nem sikerült megcsinálni /:

---
+++ Ciklus/Ciklus/Otthon2/Otthon2/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int hanyszor = int.Parse(Console.ReadLine());
+int összeg = 0;
+int összegmin = hanyszor;
+int szamok = 0;
+Console.Clear();
+/*
+for (int i = 1; i <= hanyszor; i++)
+{
+    összeg += i;
+    Console.WriteLine(összeg);
+}
+Console.WriteLine(összeg);
+*/
+
+for  (int j = hanyszor; j >= 1; j--)
+{
+    szamok += 1;
+}
+összegmin -= szamok;
+Console.WriteLine(összegmin);
