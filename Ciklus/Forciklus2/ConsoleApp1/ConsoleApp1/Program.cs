404a7894cefa94205eef2752afa943a6d70e7711
Dookusz <daniel.doka@umszki.hu>
2025-10-08 12:13:02 +0200

    Cucc

--- Ciklus/Forciklus2/ConsoleApp1/ConsoleApp1/Program.cs
+++ Ciklus/Forciklus2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,7 +1,4 @@
-﻿using System.Diagnostics;
-using System.Net.WebSockets;
-
-int szam = int.Parse(Console.ReadLine());
+﻿int szam = int.Parse(Console.ReadLine());
 int prime = 0;
 
 
@@ -11,11 +8,10 @@ for (int i = 2; i < szam; i++)
     if (szam % i == 0)
     {
         Console.WriteLine($"{i}-vel/val oszahtó");
-        prime++;
+        prime = 1;
     }
 
 }
-Console.WriteLine(prime);
 if (prime == 0)
 {
     Console.WriteLine("Prím");
---
+++ Ciklus/Forciklus2/ConsoleApp1/SZámlás/Program.cs
@@ -0,0 +1,18 @@
+﻿int szam = int.Parse(Console.ReadLine());
+
+for (int i = 1; i < szam + 1; i++)
+{
+    for (int j = 1; j < szam + 1; j++)
+    {
+        int szorzat = i * j;
+
+        if (szorzat < 10)
+        {
+            Console.Write(" ");
+        }
+        Console.Write($"{szorzat} ");
+
+    }
+    Console.Write("\n");
+    
+}
