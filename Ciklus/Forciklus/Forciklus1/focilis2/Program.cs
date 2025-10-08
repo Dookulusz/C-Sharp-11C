b7aaed458766a91a9d1482c5615a4bf000d2b255
Dookusz <daniel.doka@umszki.hu>
2025-10-08 10:19:29 +0200

    for2taj

---
+++ Ciklus/Forciklus/Forciklus1/focilis2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Adj meg egy egész számot: ");
+int szam = int.Parse(Console.ReadLine());
+int eredmény = 0;
+int szamjegy;
+szam /= 10;
+
+for (int i = 1;i < 9 ; i++)
+{
+    szamjegy = szam % 10;
+
+    eredmény += szamjegy;
+
+    szam = szam / 10;
+}
+Console.WriteLine(eredmény);
